d22cb43dee651b7325475cbc034d2d18856cc559
vagrant <vagrant@vagrant.vm>
2017-02-15 00:24:06 +0000

    initialized polling app

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, flash, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+async_mode = None
+
+app = Flask(__name__)
+
+app.secret_key = "Livepollpubnub"
+
+@app.route('/')
+def index():
+    """Landing page with poll"""
+
+    return render_template("livepoll.html")
+
+
+
+
+
+
+if __name__ == "__main__":
+    # debug=True allows for use of DebugToolbarExtension downstream
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0", port=5000)
